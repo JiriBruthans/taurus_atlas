26f89cb89ae68b7048852e61c7659a10538a5f25
Jiri Bruthans <jiribruthans@zia5.cerit-sc.cz>
2023-11-14 21:01:23 +0100

    commitsk

--- data_loader.sh
+++ data_loader.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Download master matrix for messner article and metadata
+#Download master matrix for messner article and metadata from ncbi
 wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE211nnn/GSE211428/suppl/GSE211428%5Fcombined%5Fcounts.tsv.gz
 wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE211nnn/GSE211428/suppl/GSE211428%5Fcombined%5Fmeta.tsv.gz
 
