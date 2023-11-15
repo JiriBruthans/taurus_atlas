5bf084852b7e9d064c19a3ed233c3ceb04debe0e
Jiri Bruthans <jiribruthans@skirit.metacentrum.cz>
2023-11-15 20:10:45 +0100

    add dataloader

---
+++ data_loader.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Download data and metadata (combined expression matrix) from Messmer paper
+wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE211nnn/GSE211428/suppl/GSE211428%5Fcombined%5Fcounts.tsv.gz
+wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE211nnn/GSE211428/suppl/GSE211428%5Fcombined%5Fmeta.tsv.gz
